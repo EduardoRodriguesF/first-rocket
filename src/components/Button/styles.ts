f2df3f4ac0dd55bdf2162c6aa2a9717013cc60cc
EduardoRodriguesF <eduaadorfernandes@gmail.com>
2020-11-14 09:36:38 -0300

    fix: button component styles

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  width: 300px;
+  padding: 16px;
+
+  font-weight: 600;
+  font-size: 23px;
+  text-transform: uppercase;
+
+  color: #fff;
+  background: #8c58f1;
+
+  border: none;
+  border-radius: 100px;
+`;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import { Container } from './styles';
+
 import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Register: React.FC = () => {
   return (
     <div>
-      <Input name="name" placeholder="Nome completo" />
-      <Input name="cpf" placeholder="CPF" />
-      <Input name="email" placeholder="E-mail" />
-      <Input name="password" placeholder="Senha" type="password" />
+      <form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="cpf" placeholder="CPF" />
+        <Input name="email" placeholder="E-mail" />
+        <Input name="password" placeholder="Senha" />
+
+        <Button type="submit">confirmar</Button>
+      </form>
     </div>
   );
 };
