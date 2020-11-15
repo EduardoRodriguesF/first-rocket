635b9e8b61bbb30d5aff1e9586032a156ec58e81
EduardoRodriguesF <eduaadorfernandes@gmail.com>
2020-11-15 12:03:07 -0300

    fix: home eslint errors

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
